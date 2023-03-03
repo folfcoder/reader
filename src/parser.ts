ee1e2ba6510c24f2aae26cb7f56955b91c6648d1
Kai Folf <github@fcd.im>
2023-03-03 22:45:14 +0700

    Rename path to url in parsers

--- src/parser.ts
+++ src/parser.ts
@@ -1,12 +1,22 @@
 /* 📰⚡ Reader
 File: src/parser.ts
 Author: Kai Folf
-Description: Export all parsers
+Description: Parse news based on website
 License: MIT
 */
 
 import { parseKompas } from "./website/kompas";
 import { parseTribun } from "./website/tribun";
 import { parseKompasID } from "./website/kompas-id";
+import { newsData } from "./template";
 
-export { parseKompas, parseTribun, parseKompasID };
+export async function parseNews(url: string): Promise<newsData> {
+    // Clean URL
+    url = url.split("?")[0];
+
+    // Parse news
+    if (url.includes("kompas.com")) return parseKompas(url);
+    if (url.includes("kompas.id")) return parseKompasID(url);
+    if (url.includes("tribunnews.com")) return parseTribun(url);
+    return { lang: "en", title: "📰⚡ Reader: Website Not Supported!", description: "Tired of slow, ad-filled news websites? Say hello to the ad-free, lightning-fast Indonesian news reader that'll make you feel like a superhero!", imageSrc: "https://placekitten.com/720/480", imageAlt: "Enjoy this picture of a kitten!", author: "📰⚡ Reader", pubDate: new Date(), content: "<p>Reader is a work in progress, which means that not all websites are supported. Check back later!</p>" };
+}

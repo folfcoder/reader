170a8091c4622409253ce7c03346e25eae0b3166
Kai Folf <github@fcd.im>
2023-03-04 18:26:44 +0700

    Add support for liputan6.com

--- src/parser.ts
+++ src/parser.ts
@@ -8,6 +8,7 @@ License: MIT
 import { parseKompas } from "./website/kompas";
 import { parseTribun } from "./website/tribun";
 import { parseKompasID } from "./website/kompas-id";
+import { parseLiputan6 } from "./website/liputan6";
 import { newsData } from "./template";
 
 export async function parseNews(url: string): Promise<newsData> {
@@ -18,5 +19,6 @@ export async function parseNews(url: string): Promise<newsData> {
     if (url.includes("kompas.com")) return parseKompas(url);
     if (url.includes("kompas.id")) return parseKompasID(url);
     if (url.includes("tribunnews.com")) return parseTribun(url);
+    if (url.includes("liputan6.com")) return parseLiputan6(url);
     return { lang: "en", title: "📰⚡ Reader: Website Not Supported!", description: "Tired of slow, ad-filled news websites? Say hello to the ad-free, lightning-fast Indonesian news reader that'll make you feel like a superhero!", imageSrc: "https://placekitten.com/720/480", imageAlt: "Enjoy this picture of a kitten!", author: "📰⚡ Reader", pubDate: new Date(), content: "<p>Reader is a work in progress, which means that not all websites are supported. Check back later!</p>" };
 }

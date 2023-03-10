b4b6de02a1e2244cb0a39af546a4d42c4c2ea0fb
Kai Folf <github@fcd.im>
2023-03-10 22:59:18 +0700

    Update template

--- src/template.ts
+++ src/template.ts
@@ -5,6 +5,8 @@ Description: Template for news page
 License: MIT
 */
 
+import { getStatusText, StatusCode } from "hono/utils/http-status";
+
 export type newsData = {
   lang: string;
   title: string;
@@ -14,6 +16,8 @@ export type newsData = {
   author: string;
   imageAlt: string;
   content: string;
+  status: number;
+  error: string;
 };
 
 export function indexTemplate(news: []): string {
@@ -84,6 +88,20 @@ export function indexTemplate(news: []): string {
 }
 
 export function newsTemplate(data: newsData): string {
+  if (data.status !== 200) {
+    data = {
+      lang: "en",
+      title: `📰⚡ Reader: ${data.status} ${getStatusText(data.status as StatusCode)}`,
+      description: `📰⚡ Reader: ${data.status} ${getStatusText(data.status as StatusCode)}`,
+      imageSrc: "https://placekitten.com/720/480",
+      imageAlt: "Enjoy this picture of a kitten!",
+      pubDate: new Date(),
+      author: "📰⚡ Reader",
+      content: `<p>Uh oh! It looks like our code wizards accidentally let a pesky gremlin sneak into the system. Not to worry, we've apprehended the little troublemaker and are putting it in a time-out. Please take a moment to enjoy this adorable kitten pics while we straighten things out.</p><code>${data.error}</code><br><br>`,
+      status: data.status,
+      error: data.error,
+    };
+  }
   const html = `
         <!DOCTYPE html>
         <html lang="${data.lang}">
@@ -123,7 +141,9 @@ export function newsTemplate(data: newsData): string {
           <img width="750" height="500" src="${data.imageSrc}" alt="${
     data.imageAlt
   }"/>
-          ${data.content}
+          <article>
+            ${data.content}
+          </article>
           <a href="/"><< Return</a>
           <footer>
             📰⚡ Reader by <a href="https://fcd.im" target="_blank">Kai</a> | 
@@ -133,53 +153,5 @@ export function newsTemplate(data: newsData): string {
         </body>
         </html>
       `;
-  return html;
-}
-
-export function errorTemplate(error: string): string {
-  const html = `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <title>📰⚡ Reader - The Lightning-fast Indonesian News Reader</title>
-    <meta name="description" content="Read Kompas, Liputan6, Tribunnews articles without ads and distractions!">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="theme-color" content="#DDFE67">
-
-    <!-- Open Graph -->
-    <meta property="og:site_name" content="📰⚡ Reader">
-    <meta property="og:title" content="📰⚡ Reader - The Lightning-fast Indonesian News Reader">
-    <meta property="og:description" content="Read Kompas, Liputan6, Tribunnews articles without ads and distractions!">
-    <meta property="og:type" content="website">
-    <meta property="og:image" content="https://opengraph.githubassets.com/93319d7a41623d264aa57403823edc73d803e4076f9de6031273563c096d8f9b/folfcoder/reader">
-
-    <!-- Twitter -->
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:site" content="@folfcoder">
-    <meta name="twitter:title" content="📰⚡ Reader - The Lightning-fast Indonesian News Reader">
-    <meta name="twitter:description" content="Read Kompas, Liputan6, Tribunnews articles without ads and distractions!">
-    <meta name="twitter:image" content="https://opengraph.githubassets.com/93319d7a41623d264aa57403823edc73d803e4076f9de6031273563c096d8f9b/folfcoder/reader">
-    <meta name="twitter:image:alt" content="Image banner for 📰⚡ Reader.">
-
-    <link rel="dns-prefetch" href="https://cdn.jsdelivr.net/">
-    <link rel="dns-prefetch" href="https://static.cloudflareinsights.com/" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
-  </head>
-  <body>
-    <h1>📰⚡ Reader: Server Error</h1>
-    <span>${new Date().toString()}</span>
-    <hr>
-    <p><code>${error}</code></p>
-    <p>Uh oh! It looks like our code wizards accidentally let a pesky gremlin sneak into the system. Not to worry, we've apprehended the little troublemaker and are putting it in a time-out. Please take a moment to enjoy this adorable kitten pics while we straighten things out.</p>
-    <img src="https://placekitten.com/720/480" alt="Enjoy this picture of a kitten!" width="100%" />
-    <p><a href="/"><< Return</a></p>
-    <footer>
-      📰⚡ Reader by <a href="https://fcd.im" target="_blank">Kai</a> | 
-      <a href="https://gist.github.com/folfcoder/c80ebb177db1e83dd12e24432a9b58b6/raw/reader.user.js" target="_blank">Userscript</a> |
-      Built with Cloudflare Workers
-    </footer>
-  </body>
-  </html>
-  `;
   return html;
 }
